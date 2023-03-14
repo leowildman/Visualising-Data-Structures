5ca77f27d4efd75e691deba588f16515e7885a68
Leo Wildman <wildman.l@etoncollege.org.uk>
2023-03-14 15:20:30 +0000

    Initial Commit

---
+++ Visualising Data Structures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Visualising Data Structures
+//
+//  Created by Wildman, Leo (RCH) on 14/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Visualising Data Structures/Visualising_Data_StructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visualising_Data_StructuresApp.swift
+//  Visualising Data Structures
+//
+//  Created by Wildman, Leo (RCH) on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Visualising_Data_StructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
